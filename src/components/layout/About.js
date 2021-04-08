7656f32abd28712d813036478dd6e73d9fd17365
Debanjit Sengupta <rikrocks4@gmail.com>
2021-04-08 19:36:29 +0530

    Change text in About me section. Add resume. Add favicon.

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -13,7 +13,7 @@ const About = ({ about }) => {
 					A Web Developer from India
 				</h4>
 				<p className='paragraph u-margin-bottom-small'>
-					I never would have thought that programming would be so rewarding,
+					{/* I never would have thought that programming would be so rewarding,
 					until I had built a few projects with HTML, CSS and Javascript. It was
 					an ardous task at first, especially since I was just starting out. But
 					the joy and happiness that came with completing a project was
@@ -25,7 +25,15 @@ const About = ({ about }) => {
 					order to efficiently build beautiful modern websites and apps. <br />{" "}
 					Web Development has never been "just a job" for me, but a challenge to
 					improve my skills, to learn new things and create high quality
-					websites and applications.
+					websites and applications. */}
+					I welcome you to my website. I am a Front-End Developer having
+					experience in buiding full stack applications. Here you will find the
+					projects I built using the technology skills that I currently possess.
+					The first two projects are built from scratch with the designs I
+					created in Figma. <br /> I am now looking forward to work in an
+					environment where I can better utilise my skills and become a better
+					developer. <br /> If you have any questions or any queries please feel
+					free to contact me directly.
 				</p>
 				{/* <div className='description__full-story '>
 					{/* <a href=''>Read my story </a> */}
@@ -44,7 +52,14 @@ const About = ({ about }) => {
 					</li>
 				</ul>
 				<div className='description__btn '>
-					<button className=' btn btn--black'>View Resume</button>
+					<a
+						style={{ color: "#fff" }}
+						href='../../docs/DEBANJIT_SENGUPTA_Software_Developer.pdf'
+						className=' btn btn--black'
+						download
+					>
+						View Resume
+					</a>
 				</div>
 			</div>
 		</section>
